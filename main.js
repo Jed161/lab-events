d60e59ddc975ebaa3bda2d29a31c614cbf8780e5
James Edmond <jamesedmond@pursuit.org>
2023-03-19 12:17:31 -0400

    committing lab-events

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let colorButtons = document.querySelectorAll(".color")
+//console.log(colorButtons)
+
+let currentColorSquare = document.querySelector("#current-color")
+//console.log(currentColor)
+
+colorButtons[0].addEventListener("click", (event) => {
+  //console.log(event.target)
+  //console.log(event.target.style.background)
+  currentColorSquare.style.background = event.target.style.background 
+})
+
+for (const colorButton of colorButtons) {
+  colorButton.addEventListener("click", (event) => {
+    currentColorSquare.style.background = event.target.style.background
+  })
+}
+
+let colorCells = document.querySelectorAll(".cell")
+
+for (const cell of colorCells) {
+  cell.addEventListener("click", (event) => {
+    cell.style.background = currentColorSquare.style.background
+  })
+}
